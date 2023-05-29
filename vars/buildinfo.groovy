b91d73e07dc15e372972ad925421d8bd98dc3bd4
Harsha-1919 <134038651+Harsha-1919@users.noreply.github.com>
2023-05-29 22:34:58 +0530

    Update buildinfo.groovy

--- vars/buildinfo.groovy
+++ vars/buildinfo.groovy
@@ -1,6 +1,16 @@
 def build_info(String jobname){
-   echo "${JOB_NAME}"
+   echo" this is the called function for getting email, login user and current job name"
+   
+    echo " email ${var_email}"
+	 //  echo "env.BUILD_NUMBER"
+	 
+	 echo " Username is ${env.USER}"
+	 echo "Username using build userid  is ${env.BUILD_USER_ID}"
+	 echo " Username is build_user ${env.BUILD_USER}"
+  echo "Current job name ${JOB_NAME} -> ${env.JOB_NAME}"
+   
+   //  echo "${JOB_NAME}"
 //  echo  "login user: ${user}"
  // echo " job name: ${jobName}"
-   echo " buildusername : ${BUILD_USER}"
+//   echo " buildusername : ${BUILD_USER}"
 }
