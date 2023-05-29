83420d4342e8f1d1352f9c85c3f3ed63235cb9b9
Harsha-1919 <134038651+Harsha-1919@users.noreply.github.com>
2023-05-29 22:18:28 +0530

    Update buildinfo.groovy

--- vars/buildinfo.groovy
+++ vars/buildinfo.groovy
@@ -1,5 +1,5 @@
-def build_info(String jobname  String param2){
-  echo "${param1} ${param2}"
+def build_info(String jobname){
+   echo "$JOB_NAME"
   echo  "login user: ${user}"
   echo " job name: ${jobName}"
 }
