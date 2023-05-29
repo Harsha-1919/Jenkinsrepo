38bfb6ee5c034c7df54c32dd90e41691f9a94956
Harsha-1919 <134038651+Harsha-1919@users.noreply.github.com>
2023-05-29 22:50:43 +0530

    Create builddetail.groovy

---
+++ vars/builddetail.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent { label 'masternodes'} 
+    stages {
+     stage ('Stage 1'){
+     steps {
+    
+        echo 'Print Build number and current login username'
+		
+		wrap([$class: 'BuildUser']) {
+                    diplay_build_user(BUILD_USER)
+                    display_node_name(NODE_NAME)
+       
+         //   echo "BUILD_NUMBER AS BELOW "
+         //   echo "Build number is ${currentBuild.number}"
+	   echo 'node1'
+	    echo " email ${var_email}"
+	 //  echo "env.BUILD_NUMBER"
+	 
+	// echo " Username is ${env.USER}"
+	 //echo "Username using build userid  is ${env.BUILD_USER_ID}"
+	 //echo " Username is build_user ${env.BUILD_USER}"
+	// echo "The ${JOB_NAME} run on ${NODE_NAME}"
+	// echo "BUILD_TRIGGER_BY: ${BUILD_TRIGGER_BY}"
+        
+    
+           }
+        }
+    }
+}
